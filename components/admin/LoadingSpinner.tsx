1e29fd361c6e38040d598563653cd39848435f8c
G-crown <olugbengastephen3@gmail.com>
2026-07-08 15:19:59 +0100

    Add LoadingSpinner component for loading indication

---
+++ components/admin/LoadingSpinner.tsx
@@ -0,0 +1,9 @@
+export default function LoadingSpinner() {
+  return (
+    <div className="flex justify-center py-10">
+
+      <div className="animate-spin rounded-full h-10 w-10 border-4 border-[#1A1AFF] border-t-transparent" />
+
+    </div>
+  );
+}
